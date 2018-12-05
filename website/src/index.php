185a8d1bcda0f1032871b51d181f8bd43a43897e
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-12-05 10:57:51 +0800

    Add html and consume api of python webservice

--- website/src/index.php
+++ website/src/index.php
@@ -1,2 +1,22 @@
+<html>
+<head>
+  <title>Website</title>
+</head>
+<body>
+  <h1>Welcome to my shop</h1>
+  <ul>
+    <?php
+    $json = file_get_contents('http://webservice');
+    $obj = json_decode($json);
+
+    $products = $obj->products;
+    foreach ($products as $key => $product) {
+      echo "<li>$product</li>";
+    }
+    ?>
+  </ul>
+</body>
+</html>
+
 <?php
 phpinfo();
